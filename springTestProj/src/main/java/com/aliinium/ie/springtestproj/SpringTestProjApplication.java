9d156961f7fb6f8631181bef0212e6c673493ec5
Ali <62478970+aliinium@users.noreply.github.com>
2024-03-12 23:50:41 +0330

    Create SpringBoot Test Project

---
+++ springTestProj/src/main/java/com/aliinium/ie/springtestproj/SpringTestProjApplication.java
@@ -0,0 +1,13 @@
+package com.aliinium.ie.springtestproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestProjApplication.class, args);
+    }
+
+}
---
+++ springTestProj/src/test/java/com/aliinium/ie/springtestproj/SpringTestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliinium.ie.springtestproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
