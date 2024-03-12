dddd5b5605df2f0ef8b53134e2cfd6b8c3bb7fa2
Ali <62478970+aliinium@users.noreply.github.com>
2024-03-12 23:53:44 +0330

    Update Repository

--- springTestProj/src/main/java/com/aliinium/springtestproj/SpringTestProjApplication.java
+++ springTestProj/src/main/java/com/aliinium/springtestproj/SpringTestProjApplication.java
@@ -1,4 +1,4 @@
-package com.aliinium.ie.springtestproj;
+package com.aliinium.springtestproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTestProjApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringTestProjApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestProjApplication.class, args);
+	}
 
 }
--- springTestProj/src/test/java/com/aliinium/springtestproj/SpringTestProjApplicationTests.java
+++ springTestProj/src/test/java/com/aliinium/springtestproj/SpringTestProjApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aliinium.ie.springtestproj;
+package com.aliinium.springtestproj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringTestProjApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
