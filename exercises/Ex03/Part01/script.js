ab1a5b9a31b5283630988b71c56ceee6b9d46631
Ali <62478970+aliinium@users.noreply.github.com>
2024-04-19 16:06:14 +0330

    Update script.js (add regex) and element styles

--- exercises/Ex03/Part01/script.js
+++ exercises/Ex03/Part01/script.js
@@ -1,33 +1,72 @@
-let patternCodeMelli = /123/;
-let patternSerialSh = "";
-let patternBirthDate = "";
-let patternAddress = "";
+let patternCodeMelli = /^(?:\d{10}|[1-9]\d{0,5})$/;
+let patternSerialSh = /^\d{1,2}\/[A-Za-z]-\d{6}$/;
+let patternBirthDate = /^(\d{4}\/0[1-6]\/((0[1-9]|(1[0-9]|(2[0-9]|(3[0-1]))))))|(\d{4}\/((0[7-9])|(1[0-2]))\/((0[1-9]|(1[0-9]|(2[0-9]|([3][0]))))))$/;
+let patternAddress = /123/;
 
 function checkCodeMelliFunc() {
     var txtCodeMelli = document.getElementById("txtCodeMelli");
-    var txtCodeMelli_acceptable = document.getElementById("txtCodeMelli_acceptable");
-    var txtCodeMelli_failed = document.getElementById("txtCodeMelli_failed");
+    var txtCodeMelli_alert = document.getElementById("txtCodeMelli_Alert");
 
-    let patternCheckResult = patternCodeMelli.test(txtCodeMelli.value)
+    var patternCheckResult = patternCodeMelli.test(txtCodeMelli.value)
 
     if (patternCheckResult) {
-        txtCodeMelli_acceptable.style.visibility = "visible";
-        txtCodeMelli_failed.style.visibility = "hidden";
+        txtCodeMelli_alert.innerHTML = "✔";
+        txtCodeMelli_alert.style.color = "#0D9276";
     } else if (patternCheckResult == false && txtCodeMelli.value.length != 0) {
-        txtCodeMelli_acceptable.style.visibility = "hidden";
-        txtCodeMelli_failed.style.visibility = "visible";
+        txtCodeMelli_alert.innerHTML = "✘";
+        txtCodeMelli_alert.style.color = "#E72929";
     } else {
-        txtCodeMelli_acceptable.style.visibility = "hidden";
-        txtCodeMelli_failed.style.visibility = "hidden";
+        txtCodeMelli_alert.innerHTML = "";
     }
 }
 
+function checkSerialShFunc() {
+    var txtSerialSh = document.getElementById("txtSerialSh");
+    var txtSerialSh_alert = document.getElementById("txtSerialSh_Alert");
 
-document.getElementById("txtSerialSh_failed").style.visibility = "hidden";
-document.getElementById("txtSerialSh_acceptable").style.visibility = "hidden";
+    var patternCheckResult = patternSerialSh.test(txtSerialSh.value)
 
-document.getElementById("txtBirthDate_failed").style.visibility = "hidden";
-document.getElementById("txtBirthDate_acceptable").style.visibility = "hidden";
+    if (patternCheckResult) {
+        txtSerialSh_alert.innerHTML = "✔";
+        txtSerialSh_alert.style.color = "#0D9276";
+    } else if (patternCheckResult == false && txtSerialSh.value.length != 0) {
+        txtSerialSh_alert.innerHTML = "✘";
+        txtSerialSh_alert.style.color = "#E72929";
+    } else {
+        txtSerialSh_alert.innerHTML = "";
+    }
+}
 
-document.getElementById("txtAddress_failed").style.visibility = "hidden";
-document.getElementById("txtAddress_acceptable").style.visibility = "hidden";
+function checkBirthDateFunc() {
+    var txtBirthDate = document.getElementById("txtBirthDate");
+    var txtBirthDate_alert = document.getElementById("txtBirthDate_Alert");
+
+    var patternCheckResult = patternBirthDate.test(txtBirthDate.value)
+
+    if (patternCheckResult) {
+        txtBirthDate_alert.innerHTML = "✔";
+        txtBirthDate_alert.style.color = "#0D9276";
+    } else if (patternCheckResult == false && txtBirthDate.value.length != 0) {
+        txtBirthDate_alert.innerHTML = "✘";
+        txtBirthDate_alert.style.color = "#E72929";
+    } else {
+        txtBirthDate_alert.innerHTML = "";
+    }
+}
+
+function checkAddressFunc() {
+    var txtAddress = document.getElementById("txtAddress");
+    var txtAddress_alert = document.getElementById("txtAddress_Alert");
+
+    var patternCheckResult = patternAddress.test(txtAddress.value)
+
+    if (patternCheckResult) {
+        txtAddress_alert.innerHTML = "✔";
+        txtAddress_alert.style.color = "#0D9276";
+    } else if (patternCheckResult == false && txtAddress.value.length != 0) {
+        txtAddress_alert.innerHTML = "✘";
+        txtAddress_alert.style.color = "#E72929";
+    } else {
+        txtAddress_alert.innerHTML = "";
+    }
+}
